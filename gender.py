78eafbe3b7f0cbf31483284da3e3a0a95042da26
blinx001 <blinxcavalli@gmail.com>
2024-03-04 14:48:55 +0100

    notmuch

---
+++ gender.py
@@ -0,0 +1,11 @@
+Gender = (input ("what is your gender (Male or Female? ")).lower()
+print (Gender)
+if Gender == ('male'):
+    print ("You are a boy/man ")
+elif Gender == ('m'):
+    print ("You are a boy/man ")
+elif Gender == ('female'):
+    print ('You are a girl/woman')
+elif Gender == ('f'):
+    print ('You are a girl/woman')
+else:print ("Invalid request")
